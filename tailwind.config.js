a9daa1b2830efc390dd602d97ac76275fbbf94b6
nao-sakura <osawa_nao@sakuratravel.jp>
2023-11-21 14:38:18 +0900

    コーディング

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    
+    extend: {
+      colors: {
+        'text-Primery_main': '#F4693A',
+        'text-Primery_red': '#FF473F',
+        'text-Primery_red': '#FF473F',
+      },
+    },
+  },
+  plugins: [],
+}
+
